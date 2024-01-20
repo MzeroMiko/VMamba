e1de93c51a5a935b849ef1e361c2e55cae52512e
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2024-01-20 10:12:50 +0800

    Update __init__.py

--- classification/__init__.py
+++ classification/__init__.py
@@ -1,3 +1,3 @@
 # based on https://github.com/microsoft/Swin-Transformer#20240103
-# main.py and utils/utils_ema.py is modified from https://github.com/microsoft/Swin-Transformer#20240103, based on https://github.com/facebookresearch/ConvNeXt#20240103
+# main.py is modified from https://github.com/microsoft/Swin-Transformer#20240103, based on https://github.com/facebookresearch/ConvNeXt#20240103
 
