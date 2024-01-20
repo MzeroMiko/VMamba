b04bcbb0513d79c8141d5ed50c6083311d9f2c83
田运杰 <48153283+sunsmarterjie@users.noreply.github.com>
2024-01-20 10:20:35 +0800

    Delete segmentation/configs/.ignore directory

--- segmentation/configs/.ignore/point_rend/pointrend_r101_4xb4-160k_ade20k-512x512.py
+++
@@ -1,2 +0,0 @@
-_base_ = './pointrend_r50_4xb4-160k_ade20k-512x512.py'
-model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
--- segmentation/configs/.ignore/stdc/stdc2_4xb12-80k_cityscapes-512x1024.py
+++
@@ -1,2 +0,0 @@
-_base_ = './stdc1_4xb12-80k_cityscapes-512x1024.py'
-model = dict(backbone=dict(backbone_cfg=dict(stdc_type='STDCNet2')))
