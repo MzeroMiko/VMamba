26e1f67b8bfe77b21630295f75c9caf01fef64cf
MzeroMiko <3496274007@qq.com>
2024-02-21 14:46:43 +0800

    update

--- selective_scan/csrc/selective_scan/cusndstate/selective_scan_core_fwd.cu
+++ selective_scan/csrc/selective_scan/cusndstate/selective_scan_core_fwd.cu
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Copyright (c) 2023, Tri Dao.
  ******************************************************************************/
-#include "../selective_scan_fwd_kernel_nstate.cuh"
+#include "../selective_scan_fwd_kernel_ndstate.cuh"
 
 template void selective_scan_fwd_cuda<1, float, float>(SSMParamsBase &params, cudaStream_t stream);
 template void selective_scan_fwd_cuda<1, at::Half, float>(SSMParamsBase &params, cudaStream_t stream);
