0dc07d11e6e01fddb2c9a17d50837cff64ed3aa6
MzeroMiko <3496274007@qq.com>
2024-02-22 23:31:24 +0800

    update

--- detection/configs/vssm/mask_rcnn_vssm_fpn_coco_small.py
+++ detection/configs/vssm/mask_rcnn_vssm_fpn_coco_small.py
@@ -5,8 +5,6 @@ _base_ = [
 model = dict(
     backbone=dict(
         type='MM_VSSM',
-        depths=(2, 2, 27, 2),
-        dims=96,
         out_indices=(0, 1, 2, 3),
         pretrained="../../ckpts/classification/outs/vssm/vssmsmall/vssmsmall_dp03_ckpt_epoch_238.pth",
         # copied from classification/configs/vssm/vssm_small_224.yaml
