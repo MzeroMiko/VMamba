e0eeeb84b500990c0e6da0991425c7dc7eb4c755
MzeroMiko <3496274007@qq.com>
2024-02-25 15:23:44 +0800

    update

--- selective_scan/test_selective_scan.py
+++ selective_scan/test_selective_scan.py
@@ -227,10 +227,86 @@ def selective_scan_ref(u, delta, A, B, C, D=None, z=None, delta_bias=None, delta
     return out if not return_last_state else (out, last_state)
 
 
-# MODE = "mamba_ssm"
+def selective_scan_ref_v2(u, delta, A, B, C, D=None, z=None, delta_bias=None, delta_softplus=False,
+                      return_last_state=False):
+    """
+    u: r(B D L)
+    delta: r(B D L)
+    A: c(D N) or r(D N)
+    B: c(D N) or r(B N L) or r(B N 2L) or r(B G N L) or (B G N L)
+    C: c(D N) or r(B N L) or r(B N 2L) or r(B G N L) or (B G N L)
+    D: r(D)
+    z: r(B D L)
+    delta_bias: r(D), fp32
+
+    out: r(B D L)
+    last_state (optional): r(B D dstate) or c(B D dstate)
+    """
+    dtype_in = u.dtype
+    A = A.to(dtype_in)
+    B = B.to(dtype_in)
+    C = C.to(dtype_in)
+    D = D.to(dtype_in) if D is not None else None
+    z = z.to(dtype_in) if z is not None else None
+    delta = delta.to(dtype_in) if delta is not None else None
+    delta_bias = delta_bias.to(dtype_in) if delta_bias is not None else None
+
+    if delta_bias is not None:
+        delta = delta + delta_bias[..., None]
+    if delta_softplus:
+        delta = F.softplus(delta)
+    batch, dim, dstate = u.shape[0], A.shape[0], A.shape[1]
+    is_variable_B = B.dim() >= 3
+    is_variable_C = C.dim() >= 3
+    if A.is_complex():
+        if is_variable_B:
+            B = torch.view_as_complex(rearrange(B, "... (L two) -> ... L two", two=2))
+        if is_variable_C:
+            C = torch.view_as_complex(rearrange(C, "... (L two) -> ... L two", two=2))
+    x = A.new_zeros((batch, dim, dstate))
+    ys = []
+    deltaA = torch.exp(torch.einsum('bdl,dn->bdln', delta, A))
+    if not is_variable_B:
+        deltaB_u = torch.einsum('bdl,dn,bdl->bdln', delta, B, u)
+    else:
+        if B.dim() == 3:
+            deltaB_u = torch.einsum('bdl,bnl,bdl->bdln', delta, B, u)
+        else:
+            B = repeat(B, "B G N L -> B (G H) N L", H=dim // B.shape[1])
+            deltaB_u = torch.einsum('bdl,bdnl,bdl->bdln', delta, B, u)
+    if is_variable_C and C.dim() == 4:
+        C = repeat(C, "B G N L -> B (G H) N L", H=dim // C.shape[1])
+    last_state = None
+    for i in range(u.shape[2]):
+        x = deltaA[:, :, i] * x + deltaB_u[:, :, i]
+        if not is_variable_C:
+            y = torch.einsum('bdn,dn->bd', x, C)
+        else:
+            if C.dim() == 3:
+                y = torch.einsum('bdn,bn->bd', x, C[:, :, i])
+            else:
+                y = torch.einsum('bdn,bdn->bd', x, C[:, :, :, i])
+        if i == u.shape[2] - 1:
+            last_state = x
+        if y.is_complex():
+            y = y.real * 2
+        ys.append(y)
+    y = torch.stack(ys, dim=2) # (batch dim L)
+    out = y if D is None else y + u * rearrange(D, "d -> d 1")
+    if z is not None:
+        out = out * F.silu(z)
+    out = out.to(dtype=dtype_in)
+    return out if not return_last_state else (out, last_state.float())
+
+
+def selective_scan_fn(u, delta, A, B, C, D=None, z=None, delta_bias=None, delta_softplus=False, return_last_state=False, *args, **kwargs):
+    return selective_scan_ref_v2(u, delta, A, B, C, D, z, delta_bias, delta_softplus, return_last_state)
+
+# MODE = None
+MODE = "mamba_ssm"
 # MODE = "sscore"
 # MODE = "sstest"
-MODE = "mamba_ssm_sscore" # 1344 items pass
+# MODE = "mamba_ssm_sscore" # 1344 items pass
 # MODE = "mamba_ssm_sscorendstate" # 1344 items pass
 
 if MODE in ["mamba_ssm"]:
@@ -261,7 +337,8 @@ elif MODE in ["mamba_ssm_sscorendstate"]:
     selective_scan_fn = build_selective_scan_fn(selective_scan_cuda_core, mode="sscorendstate")
     selective_scan_ref = build_selective_scan_fn(selective_scan_cuda, mode="mamba_ssm")
 else:
-    raise NotImplementedError
+    selective_scan_cuda = None
+
 
 print("use MODE:", MODE)
 DSTATE = [1]
@@ -269,6 +346,8 @@ DIM = [768]
 BATCHSIZE = [2]
 # DSTATE = [1] if MODE in ["mamba_ssm_sscorendstate", "sscorendstate"] else [8]
 NROWS = [1,2,3,4]
+IDTYPE = False
+IDTYPE = True
 
 # @pytest.mark.parametrize('wtype', [torch.float32, torch.complex64])
 @pytest.mark.parametrize('wtype', [torch.float32])
@@ -291,6 +370,7 @@ NROWS = [1,2,3,4]
 @pytest.mark.parametrize("dstate", DSTATE)
 def test_selective_scan(is_variable_B, is_variable_C, varBC_groups, has_D, has_z, has_delta_bias,
                         delta_softplus, return_last_state, seqlen, itype, wtype, nrows, batch_size, dim, dstate):
+    wtype = itype if IDTYPE else wtype
     print(f'method: {selective_scan_cuda}')
     if varBC_groups > 1 and (not is_variable_B or not is_variable_C):
         pytest.skip()  # This config is not applicable
