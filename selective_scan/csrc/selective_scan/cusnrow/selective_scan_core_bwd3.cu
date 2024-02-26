a989c29fe79f31f5050aa70420b8f26def2093fb
MzeroMiko <3496274007@qq.com>
2024-02-26 09:40:09 +0800

    update

--- selective_scan/csrc/selective_scan/cusnrow/selective_scan_core_bwd3.cu
+++ selective_scan/csrc/selective_scan/cusnrow/selective_scan_core_bwd3.cu
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Copyright (c) 2023, Tri Dao.
  ******************************************************************************/
-#include "../selective_scan_bwd_kernel_nrow.cuh"
+#include "selective_scan_bwd_kernel_nrow.cuh"
 
 template void selective_scan_bwd_cuda<3, float, float>(SSMParamsBwd &params, cudaStream_t stream);
 template void selective_scan_bwd_cuda<3, at::Half, float>(SSMParamsBwd &params, cudaStream_t stream);
--- selective_scan/csrc/selective_scan/cusnrow/selective_scan_core_fwd2.cu
+++ selective_scan/csrc/selective_scan/cusnrow/selective_scan_core_fwd2.cu
@@ -1,7 +1,7 @@
 /******************************************************************************
  * Copyright (c) 2023, Tri Dao.
  ******************************************************************************/
-#include "../selective_scan_fwd_kernel_nrow.cuh"
+#include "selective_scan_fwd_kernel_nrow.cuh"
 
 template void selective_scan_fwd_cuda<2, float, float>(SSMParamsBase &params, cudaStream_t stream);
 template void selective_scan_fwd_cuda<2, at::Half, float>(SSMParamsBase &params, cudaStream_t stream);
