2f6be17bd97e1f372077129fc4955a5ec5c28cbb
MzeroMiko <3496274007@qq.com>
2024-03-04 00:53:05 +0800

    update

---
+++ detection/configs/vssm1/mask_rcnn_vssm_fpn_coco_base.py
@@ -0,0 +1,31 @@
+_base_ = [
+    '../swin/mask-rcnn_swin-t-p4-w7_fpn_1x_coco.py'
+]
+
+model = dict(
+    backbone=dict(
+        type='MM_VSSM',
+        out_indices=(0, 1, 2, 3),
+        pretrained="../../ckpts/classification/outs/vssm/vssmbasedp05/vssmbase_dp05_ckpt_epoch_260.pth",
+        # copied from classification/configs/vssm/vssm_base_224.yaml
+        dims=128,
+        depths=(2, 2, 15, 2),
+        ssm_d_state=1,
+        ssm_dt_rank="auto",
+        ssm_ratio=2.0,
+        ssm_conv=3,
+        ssm_conv_bias=False,
+        forward_type="v3noz",
+        mlp_ratio=4.0,
+        downsample_version="v3",
+        patchembed_version="v2",
+        drop_path_rate=0.2,
+    ),
+    neck=dict(in_channels=[128, 256, 512, 1024]),
+)
+
+# too big
+train_dataloader = dict(batch_size=1) # as gpus=16
+
+
+
---
+++ detection/configs/vssm1/mask_rcnn_vssm_fpn_coco_small.py
@@ -0,0 +1,27 @@
+_base_ = [
+    '../swin/mask-rcnn_swin-t-p4-w7_fpn_1x_coco.py'
+]
+
+model = dict(
+    backbone=dict(
+        type='MM_VSSM',
+        out_indices=(0, 1, 2, 3),
+        pretrained="../../ckpts/classification/outs/vssm/vssmsmall/vssmsmall_dp03_ckpt_epoch_238.pth",
+        # copied from classification/configs/vssm/vssm_small_224.yaml
+        dims=96,
+        depths=(2, 2, 15, 2),
+        ssm_d_state=1,
+        ssm_dt_rank="auto",
+        ssm_ratio=2.0,
+        ssm_conv=3,
+        ssm_conv_bias=False,
+        forward_type="v3noz",
+        mlp_ratio=4.0,
+        downsample_version="v3",
+        patchembed_version="v2",
+        drop_path_rate=0.2,
+    ),
+)
+
+# train_dataloader = dict(batch_size=2) # as gpus=8
+
