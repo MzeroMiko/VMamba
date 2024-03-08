ab555da7dcc191b59de5c2e9836ae2e0ba820867
MzeroMiko <3496274007@qq.com>
2024-03-08 16:08:11 +0800

    update cross scan with triton

--- classification/models/vmamba.py
+++ classification/models/vmamba.py
@@ -15,6 +15,137 @@ from timm.models.layers import DropPath, trunc_normal_
 from fvcore.nn import FlopCountAnalysis, flop_count_str, flop_count, parameter_count
 DropPath.__repr__ = lambda self: f"timm.DropPath({self.drop_prob})"
 
+# triton cross scan, 2x speed than pytorch implementation =========================
+import torch
+import triton
+import triton.language as tl
+
+@triton.jit
+def triton_cross_scan(
+    x, # (B, C, H, W)
+    y, # (B, 4, C, H, W)
+    BC: tl.constexpr,
+    BH: tl.constexpr,
+    BW: tl.constexpr,
+    DC: tl.constexpr,
+    DH: tl.constexpr,
+    DW: tl.constexpr,
+    NH: tl.constexpr,
+    NW: tl.constexpr,
+):
+    i_hw, i_c, i_b = tl.program_id(0), tl.program_id(1), tl.program_id(2)
+    i_h, i_w = (i_hw // NW), (i_hw % NW)
+    _mask_h = (i_h * BH + tl.arange(0, BH)) < DH
+    _mask_w = (i_w * BW + tl.arange(0, BW)) < DW
+    _mask_hw = _mask_h[:, None] & _mask_w[None, :]
+    _for_C = min(DC - i_c * BC, BC)
+
+    _tmp0 = i_c * BC * DH * DW
+    _tmp1 = DC * DH * DW
+    _tmp2 = _tmp0 + i_h * BH * DW  + tl.arange(0, BH)[:, None] * DW + i_w * BW + tl.arange(0, BW)[None, :]
+    p_x = x + i_b * _tmp1 + _tmp2
+    p_y1 = y + i_b * 4 * _tmp1 + _tmp2 # same
+    p_y2 = y + i_b * 4 * _tmp1 + _tmp1 + _tmp0 + i_w * BW * DH + tl.arange(0, BW)[None, :] * DH + i_h * BH + tl.arange(0, BH)[:, None]  # trans
+    p_y3 = y + i_b * 4 * _tmp1 + 2 * _tmp1 + _tmp0 + (NH - i_h - 1) * BH * DW  + (BH - 1 - tl.arange(0, BH)[:, None]) * DW + (NW - i_w - 1) * BW + (BW - 1 - tl.arange(0, BW)[None, :]) + (DH - NH * BH) * DW + (DW - NW * BW) # flip
+    p_y4 = y + i_b * 4 * _tmp1 + 3 * _tmp1 + _tmp0 + (NW - i_w - 1) * BW * DH  + (BW - 1 - tl.arange(0, BW)[None, :]) * DH + (NH - i_h - 1) * BH + (BH - 1 - tl.arange(0, BH)[:, None]) + (DH - NH * BH) + (DW - NW * BW) * DH  # trans + flip
+
+    for idxc in range(_for_C):
+        _idx = idxc * DH * DW
+        _x = tl.load(p_x + _idx, mask=_mask_hw)
+        tl.store(p_y1 + _idx, _x, mask=_mask_hw)
+        tl.store(p_y2 + _idx, _x, mask=_mask_hw)
+        tl.store(p_y3 + _idx, _x, mask=_mask_hw)
+        tl.store(p_y4 + _idx, _x, mask=_mask_hw)
+
+
+@triton.jit
+def triton_cross_merge(
+    x, # (B, C, H, W)
+    y, # (B, 4, C, H, W)
+    BC: tl.constexpr,
+    BH: tl.constexpr,
+    BW: tl.constexpr,
+    DC: tl.constexpr,
+    DH: tl.constexpr,
+    DW: tl.constexpr,
+    NH: tl.constexpr,
+    NW: tl.constexpr,
+):
+    i_hw, i_c, i_b = tl.program_id(0), tl.program_id(1), tl.program_id(2)
+    i_h, i_w = (i_hw // NW), (i_hw % NW)
+    _mask_h = (i_h * BH + tl.arange(0, BH)) < DH
+    _mask_w = (i_w * BW + tl.arange(0, BW)) < DW
+    _mask_hw = _mask_h[:, None] & _mask_w[None, :]
+    _for_C = min(DC - i_c * BC, BC)
+
+    _tmp0 = i_c * BC * DH * DW
+    _tmp1 = DC * DH * DW
+    _tmp2 = _tmp0 + i_h * BH * DW  + tl.arange(0, BH)[:, None] * DW + i_w * BW + tl.arange(0, BW)[None, :]
+    p_x = x + i_b * _tmp1 + _tmp2
+    p_y1 = y + i_b * 4 * _tmp1 + _tmp2 # same
+    p_y2 = y + i_b * 4 * _tmp1 + _tmp1 + _tmp0 + i_w * BW * DH + tl.arange(0, BW)[None, :] * DH + i_h * BH + tl.arange(0, BH)[:, None]  # trans
+    p_y3 = y + i_b * 4 * _tmp1 + 2 * _tmp1 + _tmp0 + (NH - i_h - 1) * BH * DW  + (BH - 1 - tl.arange(0, BH)[:, None]) * DW + (NW - i_w - 1) * BW + (BW - 1 - tl.arange(0, BW)[None, :]) + (DH - NH * BH) * DW + (DW - NW * BW) # flip
+    p_y4 = y + i_b * 4 * _tmp1 + 3 * _tmp1 + _tmp0 + (NW - i_w - 1) * BW * DH  + (BW - 1 - tl.arange(0, BW)[None, :]) * DH + (NH - i_h - 1) * BH + (BH - 1 - tl.arange(0, BH)[:, None]) + (DH - NH * BH) + (DW - NW * BW) * DH  # trans + flip
+
+    for idxc in range(_for_C):
+        _idx = idxc * DH * DW
+        _y1 = tl.load(p_y1 + _idx, mask=_mask_hw)
+        _y2 = tl.load(p_y2 + _idx, mask=_mask_hw)
+        _y3 = tl.load(p_y3 + _idx, mask=_mask_hw)
+        _y4 = tl.load(p_y4 + _idx, mask=_mask_hw)
+        tl.store(p_x + _idx, _y1 + _y2 + _y3 + _y4, mask=_mask_hw)
+
+
+class CrossScanTriton(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x: torch.Tensor):
+        B, C, H, W = x.shape
+        B, C, H, W = int(B), int(C), int(H), int(W)
+        BC, BH, BW = min(triton.next_power_of_2(C), 2), min(triton.next_power_of_2(H), 32), min(triton.next_power_of_2(W), 32)
+        NH, NW, NC = triton.cdiv(H, BH), triton.cdiv(W, BW), triton.cdiv(C, BC)
+        ctx.shape = (B, C, H, W)
+        ctx.triton_shape = (BC, BH, BW, NC, NH, NW)
+        x = x.contiguous()
+        y = x.new_empty((B, 4, C, H, W))
+        triton_cross_scan[(NH * NW, NC, B)](x, y, BC, BH, BW, C, H, W, NH, NW)
+        return y.view(B, 4, C, -1)
+    
+    @staticmethod
+    def backward(ctx, y: torch.Tensor):
+        # out: (b, k, d, l)
+        B, C, H, W = ctx.shape
+        BC, BH, BW, NC, NH, NW = ctx.triton_shape
+        y = y.contiguous().view(B, 4, C, H, W)
+        x = y.new_empty((B, C, H, W))
+        triton_cross_merge[(NH * NW, NC, B)](x, y, BC, BH, BW, C, H, W, NH, NW)
+        return x
+
+
+class CrossMergeTriton(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, y: torch.Tensor):
+        B, K, C, H, W = y.shape
+        B, C, H, W = int(B), int(C), int(H), int(W)
+        BC, BH, BW = min(triton.next_power_of_2(C), 2), min(triton.next_power_of_2(H), 32), min(triton.next_power_of_2(W), 32)
+        NH, NW, NC = triton.cdiv(H, BH), triton.cdiv(W, BW), triton.cdiv(C, BC)
+        ctx.shape = (B, C, H, W)
+        ctx.triton_shape = (BC, BH, BW, NC, NH, NW)
+        y = y.contiguous().view(B, 4, C, H, W)
+        x = y.new_empty((B, C, H, W))
+        triton_cross_merge[(NH * NW, NC, B)](x, y, BC, BH, BW, C, H, W, NH, NW)
+        return x.view(B, C, -1)
+    
+    @staticmethod
+    def backward(ctx, x: torch.Tensor):
+        # out: (b, d, l)
+        B, C, H, W = ctx.shape
+        BC, BH, BW, NC, NH, NW = ctx.triton_shape
+        x = x.contiguous()
+        y = x.new_empty((B, 4, C, H, W))
+        triton_cross_scan[(NH * NW, NC, B)](x, y, BC, BH, BW, C, H, W, NH, NW)
+        return y
+
+
 # import selective scan ==============================
 try:
     import selective_scan_cuda_oflex
@@ -362,7 +493,7 @@ class CrossMerge_Ab_1direction(torch.autograd.Function):
 
 
 # =============
-
+# Note: we did not use csm_triton in and before vssm1_0230, we used pytorch version !
 def cross_selective_scan(
     x: torch.Tensor=None, 
     x_proj_weight: torch.Tensor=None,
@@ -383,8 +514,8 @@ def cross_selective_scan(
     ssoflex=True, # True: out fp32 in SSOflex; else, SSOflex is the same as SSCore
     # ==============================
     SelectiveScan=None,
-    CrossScan=CrossScan,
-    CrossMerge=CrossMerge,
+    CrossScan=CrossScanTriton,
+    CrossMerge=CrossMergeTriton,
 ):
     # out_norm: whatever fits (B, L, C); LayerNorm; Sigmoid; Softmax(dim=1);...
 
@@ -776,6 +907,13 @@ class SS2D(nn.Module):
         return out
 
 
+try:
+    from .ss2d_ablations import SS2D_TEST
+    SS2D = SS2D_TEST
+    print("DEBUG MODE ===========================================")
+except:
+    ...
+
 class Permute(nn.Module):
     def __init__(self, *args):
         super().__init__()
@@ -837,15 +975,9 @@ class VSSBlock(nn.Module):
         self.use_checkpoint = use_checkpoint
         self.post_norm = post_norm
 
-        try:
-            from ss2d_ablations import SS2DDev
-            _SS2D = SS2DDev if forward_type.startswith("dev") else SS2D
-        except:
-            _SS2D = SS2D
-
         if self.ssm_branch:
             self.norm = norm_layer(hidden_dim)
-            self.op = _SS2D(
+            self.op = SS2D(
                 d_model=hidden_dim, 
                 d_state=ssm_d_state, 
                 ssm_ratio=ssm_ratio,
@@ -1423,6 +1555,34 @@ def check_vssm1_ssoflex_equals_mambassm():
     print("init miss align", miss_align) # init miss align 0
 
 
+def check_csm_triton():
+    B, C, H, W = 128, 192, 56, 57
+    dtype=torch.float16
+    dtype=torch.float32
+    x = torch.randn((B, C, H, W), dtype=dtype, device=torch.device("cuda")).requires_grad_(True)
+    y = torch.randn((B, 4, C, H, W), dtype=dtype, device=torch.device("cuda")).requires_grad_(True)
+    x1 = x.clone().detach().requires_grad_(True)
+    y1 = y.clone().detach().requires_grad_(True)
+    o0 = CrossScan.apply(x)
+    o1 = CrossScanTriton.apply(x1)
+    o0.backward(y.view(B, 4, C, H * W))
+    o1.backward(y.view(B, 4, C, H * W))
+    print((o0 - o1).abs().max())
+    print((x.grad - x1.grad).abs().max())
+    o0 = CrossMerge.apply(y)
+    o1 = CrossMergeTriton.apply(y1)
+    o0.backward(x.view(B, C, H * W))
+    o1.backward(x.view(B, C, H * W))
+    print((o0 - o1).abs().max())
+    print((y.grad - y1.grad).abs().max())
+
+    res0 = triton.testing.do_bench(lambda :CrossScan.apply(x))
+    res1 = triton.testing.do_bench(lambda :CrossScanTriton.apply(x))
+    res2 = triton.testing.do_bench(lambda :CrossMerge.apply(y))
+    res3 = triton.testing.do_bench(lambda :CrossMergeTriton.apply(y))
+    print(res0, res1, res2, res3)
+    breakpoint()
+
 def check_vssblock():
     import triton
     from torchvision.models.vision_transformer import EncoderBlock
@@ -1606,7 +1766,8 @@ if __name__ == "__main__":
     # print(VSSM(forward_type="v2").flops())
     # print(VSSM(forward_type="v2nozact").flops())
 
-    check_vssm1_ssoflex_equals_mambassm()
+    # check_vssm1_ssoflex_equals_mambassm()
+    check_csm_triton()
 
     
 
