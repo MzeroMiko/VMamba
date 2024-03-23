643fe1e2d74cc4238ae1d33cb432d471ee431d61
MzeroMiko <3496274007@qq.com>
2024-03-23 23:18:50 +0800

    update

--- classification/models/vmamba.py
+++ classification/models/vmamba.py
@@ -345,14 +345,16 @@ class LayerNorm2d(nn.LayerNorm):
 
 
 class PatchMerging2D(nn.Module):
-    def __init__(self, dim, out_dim=-1, norm_layer=nn.LayerNorm, **kwargs):
+    def __init__(self, dim, out_dim=-1, norm_layer=nn.LayerNorm, channel_first=False):
         super().__init__()
         self.dim = dim
-        self.reduction = nn.Linear(4 * dim, (2 * dim) if out_dim < 0 else out_dim, bias=False)
+        Linear = Linear2d if channel_first else nn.Linear
+        self._patch_merging_pad = self._patch_merging_pad_channel_first if channel_first else self._patch_merging_pad_channel_last
+        self.reduction = Linear(4 * dim, (2 * dim) if out_dim < 0 else out_dim, bias=False)
         self.norm = norm_layer(4 * dim)
 
     @staticmethod
-    def _patch_merging_pad(x: torch.Tensor):
+    def _patch_merging_pad_channel_last(x: torch.Tensor):
         H, W, _ = x.shape[-3:]
         if (W % 2 != 0) or (H % 2 != 0):
             x = F.pad(x, (0, 0, 0, W % 2, 0, H % 2))
@@ -361,6 +363,18 @@ class PatchMerging2D(nn.Module):
         x2 = x[..., 0::2, 1::2, :]  # ... H/2 W/2 C
         x3 = x[..., 1::2, 1::2, :]  # ... H/2 W/2 C
         x = torch.cat([x0, x1, x2, x3], -1)  # ... H/2 W/2 4*C
+        return x
+
+    @staticmethod
+    def _patch_merging_pad_channel_first(x: torch.Tensor):
+        H, W = x.shape[-2:]
+        if (W % 2 != 0) or (H % 2 != 0):
+            x = F.pad(x, (0, 0, 0, W % 2, 0, H % 2))
+        x0 = x[..., 0::2, 0::2]  # ... H/2 W/2
+        x1 = x[..., 1::2, 0::2]  # ... H/2 W/2
+        x2 = x[..., 0::2, 1::2]  # ... H/2 W/2
+        x3 = x[..., 1::2, 1::2]  # ... H/2 W/2
+        x = torch.cat([x0, x1, x2, x3], 1)  # ... H/2 W/2 4*C
         return x
 
     def forward(self, x):
