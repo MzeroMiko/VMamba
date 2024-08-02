caf72a8f023d3b722a7e1d8470dabc835736cbe8
MzeroMiko <3496274007@qq.com>
2024-08-02 14:48:01 +0000

    add flops calculation for some other models

--- analyze/utils.py
+++ analyze/utils.py
@@ -767,8 +767,8 @@ class BuildModels:
         vitb = model.vit_base_s4nd
         model = import_abspy("convnext_timm", f"{os.path.dirname(__file__)}/convnexts4nd")
         ctiny = model.convnext_tiny_s4nd
-        sys.path = sys.path[1:]
         model = dict(ctiny=ctiny, vitb=vitb)[scale]()
+        sys.path = sys.path[1:]
         return model
 
     @staticmethod
@@ -1470,19 +1470,33 @@ class FLOPs:
         selective_scan_flop_jit: Callable = build.vmamba.selective_scan_flop_jit
         # flops_selective_scan_fn: Callable = build.vmamba.flops_selective_scan_fn
         # flops_selective_scan_ref: Callable = build.vmamba.flops_selective_scan_ref 
-
+        def causal_conv_1d_jit(inputs, outputs):
+            """
+            https://github.com/Dao-AILab/causal-conv1d/blob/main/causal_conv1d/causal_conv1d_interface.py
+            x: (batch, dim, seqlen) weight: (dim, width) bias: (dim,) out: (batch, dim, seqlen)
+            out = F.conv1d(x, weight.unsqueeze(1), bias, padding=width - 1, groups=dim)
+            """
+            from fvcore.nn.jit_handles import conv_flop_jit
+            return conv_flop_jit(inputs, outputs)
+        
         supported_ops={
             "aten::gelu": None, # as relu is in _IGNORED_OPS
             "aten::silu": None, # as relu is in _IGNORED_OPS
             "aten::neg": None, # as relu is in _IGNORED_OPS
             "aten::exp": None, # as relu is in _IGNORED_OPS
             "aten::flip": None, # as permute is in _IGNORED_OPS
-            "prim::PythonOp.SelectiveScanFn": selective_scan_flop_jit, # latter
-            "prim::PythonOp.SelectiveScanMamba": selective_scan_flop_jit, # latter
-            "prim::PythonOp.SelectiveScanOflex": selective_scan_flop_jit, # latter
-            "prim::PythonOp.SelectiveScanCore": selective_scan_flop_jit, # latter
-            "prim::PythonOp.SelectiveScan": selective_scan_flop_jit, # latter
-            "prim::PythonOp.SelectiveScanCuda": selective_scan_flop_jit, # latter
+            # =====================================================
+            # for mamba-ssm
+            "prim::PythonOp.CausalConv1dFn": causal_conv_1d_jit,
+            "prim::PythonOp.SelectiveScanFn": selective_scan_flop_jit,
+            # =====================================================
+            # for VMamba
+            "prim::PythonOp.SelectiveScanMamba": selective_scan_flop_jit,
+            "prim::PythonOp.SelectiveScanOflex": selective_scan_flop_jit,
+            # "prim::PythonOp.SelectiveScanCore": selective_scan_flop_jit,
+            "prim::PythonOp.SelectiveScan": selective_scan_flop_jit,
+            "prim::PythonOp.SelectiveScanCuda": selective_scan_flop_jit,
+            # =====================================================
             # "aten::scaled_dot_product_attention": ...
         }
         return supported_ops
@@ -1737,9 +1751,206 @@ class FLOPs:
         cls.fvcore_flop_count(model, input_shape=input_shape)
 
 
-class Evals:
-    ...
+
+class ExtraDev:
+    # 5.301500928 25796584
+    def flops_vim(size=224):
+        from fvcore.nn import flop_count
+
+        # FLOPs.fvcore_flop_count(BuildModels.build_vmamba(scale="tv2").cuda().eval(), input_shape=(3, size, size), show_arch=False)
+        print("vim ================================", flush=True)
+        specpath = f"{HOME}/packs/Vim/mamba-1p1p1"
+        sys.path.insert(0, specpath)
+        import mamba_ssm
+        _model = import_abspy("models_mamba", f"{HOME}/packs/Vim/vim")
+        sys.path = sys.path[1:]
+        # model = _model.vim_small_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2()
+        kwargs=dict()
+        # model = _model.VisionMamba(patch_size=16, embed_dim=384, depth=24, rms_norm=True, residual_in_fp32=True, fused_add_norm=True, final_pool_type='mean', if_abs_pos_embed=True, if_rope=False, if_rope_residual=False, bimamba_type="v2", if_cls_token=True, if_devide_out=True, use_middle_cls_token=True, **kwargs)
+        
+        # fused add norm share the same flops as naive one
+        model = _model.VisionMamba(patch_size=16, embed_dim=384, depth=24, rms_norm=True, residual_in_fp32=True, fused_add_norm=False, final_pool_type='mean', if_abs_pos_embed=True, if_rope=False, if_rope_residual=False, bimamba_type="v2", if_cls_token=True, if_devide_out=True, use_middle_cls_token=True, **kwargs)
+        vims = model.cuda().eval()
+
+        # RMSNorm share the same flops as naive one
+        # https://github.com/state-spaces/mamba/blob/v1.2.2/mamba_ssm/ops/triton/layernorm.py
+        def rms_norm_ref(x, weight, bias, residual=None, eps=1e-6, prenorm=False, upcast=False):
+            dtype = x.dtype
+            if upcast:
+                weight = weight.float()
+                bias = bias.float() if bias is not None else None
+            if upcast:
+                x = x.float()
+                residual = residual.float() if residual is not None else residual
+            if residual is not None:
+                x = (x + residual).to(x.dtype)
+            rstd = 1 / torch.sqrt((x.square()).mean(dim=-1, keepdim=True) + eps)
+            out = (x * rstd * weight) + bias if bias is not None else (x * rstd * weight)
+            out = out.to(dtype)
+            return out if not prenorm else (out, x)
+
+        def rms_forward(self, x, residual=None, prenorm=False, residual_in_fp32=False):
+            return rms_norm_ref(
+                x,
+                self.weight,
+                self.bias,
+                residual=residual,
+                eps=self.eps,
+                prenorm=prenorm,
+                upcast=residual_in_fp32,
+            )
+
+        for k, m in vims.named_modules():
+            if isinstance(m, _model.RMSNorm):
+                m.forward = partial(rms_forward, m)
+
+        input_shape = (1, 3, size, size)
+        model = vims.cuda().eval()
+        import math
+
+        def causal_conv_1d_jit(inputs, outputs):
+            """
+            https://github.com/Dao-AILab/causal-conv1d/blob/main/causal_conv1d/causal_conv1d_interface.py
+            x: (batch, dim, seqlen) weight: (dim, width) bias: (dim,) out: (batch, dim, seqlen)
+            out = F.conv1d(x, weight.unsqueeze(1), bias, padding=width - 1, groups=dim)
+            """
+            from fvcore.nn.jit_handles import conv_flop_jit
+            return conv_flop_jit(inputs, outputs)
+
+        # ONLY FOR VisionMamba
+        def MambaInnerFnNoOutProj_jit(inputs, outputs):
+            """
+            conv1d_weight = rearrange(conv1d_weight, "d 1 w -> d w")
+            x, z = xz.chunk(2, dim=1)
+            conv1d_out = causal_conv1d_cuda.causal_conv1d_fwd(x, conv1d_weight, conv1d_bias,None, True)
+            x_dbl = F.linear(rearrange(conv1d_out, 'b d l -> (b l) d'), x_proj_weight)  # (bl d)
+            delta = rearrange(delta_proj_weight @ x_dbl[:, :delta_rank].t(), "d (b l) -> b d l", l = L)
+            B = x_dbl[:, delta_rank:delta_rank + d_state]
+            B = rearrange(B, "(b l) dstate -> b 1 dstate l", l=L).contiguous()
+            C = x_dbl[:, -d_state:]
+            C = rearrange(C, "(b l) dstate -> b 1 dstate l", l=L).contiguous()
+            out, scan_intermediates, out_z = selective_scan_cuda.fwd(
+                conv1d_out, delta, A, B, C, D, z, delta_bias, delta_softplus
+            )
+            """
+            xz, conv1d_weight, conv1d_bias, x_proj_weight, delta_proj_weight, A = inputs[0:6]
+            Batch, _, L = xz.type().sizes()
+            CWidth = conv1d_weight.type().sizes()[-1]
+            H = A.type().sizes()[-1] # 16
+            Dim, R = delta_proj_weight.type().sizes()
+            assert tuple(xz.type().sizes()) == (Batch, 2 * Dim, L)
+            assert tuple(conv1d_weight.type().sizes()) == (Dim, 1, CWidth)
+            assert tuple(x_proj_weight.type().sizes()) == (R + H + H, Dim)
+            assert tuple(A.type().sizes()) == (Dim, H)
+
+            with_Z = True
+            with_D = False
+            if "D" in inputs[6].debugName():
+                assert tuple(inputs[6].type().sizes()) == (Dim,)
+                with_D = True
+
+            flops = 0
+            flops += Batch * (Dim * L) * CWidth # causal_conv1d_cuda.causal_conv1d_fwd
+            flops += Batch * (Dim * L) * (R + H + H) # x_dbl = F.linear(...
+            flops += Batch * (Dim * R) * (L) # delta_proj_weight @ x_dbl[:, :delta_rank]
+            
+            # https://github.com/state-spaces/mamba/issues/110
+            flops = 9 * Batch * L * Dim * H
+            if with_D:
+                flops += Batch * Dim * L
+            if with_Z:
+                flops += Batch * Dim * L  
+
+            return flops
+
+        supported_ops={
+            "aten::gelu": None, # as relu is in _IGNORED_OPS
+            "aten::silu": None, # as relu is in _IGNORED_OPS
+            "aten::neg": None, # as relu is in _IGNORED_OPS
+            "aten::exp": None, # as relu is in _IGNORED_OPS
+            "aten::flip": None, # as permute is in _IGNORED_OPS
+            "prim::PythonOp.CausalConv1dFn": causal_conv_1d_jit,
+            "prim::PythonOp.MambaInnerFnNoOutProj": MambaInnerFnNoOutProj_jit,
+        }
+        inputs = (torch.randn(input_shape).to(next(model.parameters()).device),)
+        Gflops, unsupported = flop_count(model=model, inputs=inputs, supported_ops=supported_ops)
+        print(sum(Gflops.values()), sum([p.numel() for _, p in model.named_parameters()]))    
+
+    # 5.162112298177406 30007057
+    # 17.069485400571516 91157224
+    def flops_s4nd(size=224):
+        import math
+        from fvcore.nn import flop_count
+
+        print("convnext-s4nd ================================", flush=True)
+        specpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "./convnexts4nd")
+        sys.path.insert(0, specpath)
+        import timm; assert timm.__version__ == "0.5.4"
+        import structured_kernels
+        model1 = import_abspy("vit_all", f"{os.path.dirname(__file__)}/convnexts4nd")
+        vitb = model1.vit_base_s4nd
+        vitb = vitb().cuda().eval()
+        model2 = import_abspy("convnext_timm", f"{os.path.dirname(__file__)}/convnexts4nd")
+        ctiny = model2.convnext_tiny_s4nd
+        ctiny = ctiny().cuda().eval()
+        sys.path = sys.path[1:]
+        # cauchy_mult makes only little difference as there's only 2-3 div operations
+        
+        def _supported_ops():
+            def aten_fft(inputs, outputs, fftop="fft"):
+                from torch.fft import fft, rfft, irfft, fftn, rfftn, irfftn 
+                inp, num, dim, norm = inputs[0:4]
+                is_complex = torch.is_complex(torch.tensor([], dtype=inputs[0].type().dtype()))
+                shape = inp.type().sizes()
+
+                torch._C.Value
+                if isinstance(dim.type(), torch._C.IntType):
+                    dim = [dim.toIValue()]
+                elif isinstance(dim.type(), torch._C.TupleType):
+                    dim = [d.toIValue() for d in dim.type().elements()]
+                elif isinstance(dim.type(), torch._C.NoneType):
+                    from torch.fft import fftn, rfftn, irfftn
+                    assert fftop == "fftn"
+                    if isinstance(num.type(), torch._C.NoneType):
+                        dim = list(range(len(shape)))
+                    elif isinstance(num.type(), torch._C.ListType):
+                        assert isinstance(num.type().getElementType(), torch._C.IntType)
+                        num_dims = len(tuple(num.node().inputs()))
+                        guess_dim = [-1 - i for i in range(num_dims)]
+                        print(f"Warning, We are not sure about this, guess the fft dim are {guess_dim}. input: {shape}, n or s: {num}, dim: {dim.type()}")
+                        dim = guess_dim
+                else:
+                    raise NotImplementedError
+                
+                flops = math.prod(shape) * math.prod([math.log2(shape[i]) for i in dim]) * (4 if is_complex else 1)
+                # print(flops, dim, is_complex, shape)
+                return flops
+            supported_ops={
+                "aten::fft_fft": aten_fft,
+                "aten::fft_rfft": aten_fft,
+                "aten::fft_rfftn": partial(aten_fft, fftop="fftn"),
+                "aten::fft_irfft": aten_fft,
+                "aten::fft_irfftn": partial(aten_fft, fftop="fftn"),
+            }
+            return supported_ops
+
+
+        # model = ctiny().cuda().eval()
+        model = ctiny
+        input_shape = (1, 3, size, size)
+        inputs = (torch.randn(input_shape).to(next(model.parameters()).device),)
+        model(inputs[0]) # to force init first
+        Gflops, unsupported = flop_count(model=model, inputs=inputs, supported_ops=_supported_ops())
+        print(sum(Gflops.values()), sum([p.numel() for _, p in model.named_parameters()]))    
+
+        # model = vitb().cuda().eval()
+        model = vitb
+        model(inputs[0]) # to force init first
+        Gflops, unsupported = flop_count(model=model, inputs=inputs, supported_ops=_supported_ops())
+        print(sum(Gflops.values()), sum([p.numel() for _, p in model.named_parameters()]))    
 
 
 if __name__ == "__main__":
+    ExtraDev.flops_vim()
+    ExtraDev.flops_s4nd()
     BuildModels.check()
